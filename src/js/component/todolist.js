65e63db34594591c6da7c398b33c5439f3eea7c9
Marcelo C Alves <marcelocalves@live.com>
2021-07-17 20:42:59 +0000

    Show List and delete item done

--- src/js/component/todolist.js
+++ src/js/component/todolist.js
@@ -30,35 +30,40 @@ const TodoList = () => {
 					Add
 				</button>
 				<ul className="list-group list-group-flush mt-2">
-					{store.todoList.map((element, index) => (
-						<li
-							className={
-								index % 2 === 0 ? "list-group-item list-group-item-secondary" : "list-group-item"
-							}
-							key={index}>
-							<div className="row">
-								<div className="col-sm-3 offset-sm-4 mr-3 text-left">
-									<span
-										className="listSpan"
-										style={{
-											fontFamily: "Yomogi, cursive",
-											fontSize: "20px"
-										}}>
-										{index + 1} - {element}
-									</span>
-								</div>
+					{store.todoList &&
+						store.todoList.map((element, index) => (
+							<li
+								className={
+									index % 2 === 0 ? "list-group-item list-group-item-secondary" : "list-group-item"
+								}
+								key={index}>
+								<div className="row">
+									<div className="col-sm-3 offset-sm-4 mr-3 text-left">
+										<span
+											className="listSpan"
+											style={{
+												fontFamily: "Yomogi, cursive",
+												fontSize: "20px"
+											}}>
+											{index + 1} - {element.label}
+										</span>
+									</div>
 
-								<div className="col-1 ml-3 text-right">
-									{/* <a className="mr-3" type="button">
+									<div className="col-1 ml-3 text-right">
+										{/* <a className="mr-3" type="button">
 										<i className="far fa-check-square" />
 									</a> */}
-									<a type="button" onClick={() => actions.deleteItem(element)}>
-										<i className="fas fa-trash-alt" />
-									</a>
+										<a
+											type="button"
+											onClick={() =>
+												actions.deleteItem(store.todoList.filter((e, i) => index !== i))
+											}>
+											<i className="fas fa-trash-alt" />
+										</a>
+									</div>
 								</div>
-							</div>
-						</li>
-					))}
+							</li>
+						))}
 				</ul>
 				{/* <footer>Item(s) Left {store.todoList.length}</footer> */}
 			</div>
--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -3,22 +3,35 @@ const getState = ({ getStore, getActions, setStore }) => {
 		store: {
 			listTitle: "No Title",
 			todoList: [],
-			checkItem: ""
+			checkItem: "",
+			todos: []
 		},
 		actions: {
 			addTitle: title => (title === "" ? setStore({ listTitle: "No title" }) : setStore({ listTitle: title })),
-			addItem: newItem => {
-				let newTodoList = getStore().todoList;
-				setStore({ todoList: [...newTodoList, newItem] });
+			getTodo: () => {
+				fetch("https://assets.breatheco.de/apis/fake/todos/user/marcelo")
+					.then(res => res.json())
+					.then(response => setStore({ todoList: response }));
 			},
 			deleteItem: element => {
-				let deleteItem = getStore().todoList;
-				deleteItem = deleteItem.filter(item => element !== item);
-				setStore({ todoList: deleteItem });
-			},
-			deleteList: () => {
-				setStore({ todoList: [] });
+				fetch("https://assets.breatheco.de/apis/fake/todos/user/marcelo", {
+					method: "PUT",
+					body: JSON.stringify(element),
+					headers: {
+						"Content-Type": "application/json"
+					}
+				})
+					.then(res => res.json())
+					.then(() => getActions().getTodo());
 			}
+			// addItem: newItem => {
+			// 	let newTodoList = getStore().todoList;
+			// 	setStore({ todoList: [...newTodoList, newItem] });
+			// },
+
+			// deleteList: () => {
+			// 	setStore({ todoList: [] });
+			// }
 		}
 	};
 };
